6d658f8af7913cd2dc98de27bdd58dc3eedc6abc
eloy_chang <eloy.chang.182@gmail.com>
2017-08-31 08:50:37 -0400

    Avances en graficos

--- Part3/Graficos.R
+++ Part3/Graficos.R
@@ -1,8 +1,11 @@
-library(ggplot2)
+library(plotly)
+
+rm(list = ls(all.names = TRUE))
+hola<- .SimBookStore(30)
 
 hola$Dia=1:30
 names(hola)<- c("Clientes","BuenosClientes","Libros","LibrosUsados","LibrosComprados",
-                "Ganancia","GananciaNeta","GananciaNetaLU","dia")
+                "Ganancia","GananciaNeta","GananciaNetaLU","Dia")
 
 #Clientes
 plot_ly(data=hola, x= ~Dia, y= ~Clientes, type = "bar", name = "Total Clientes") %>% 
@@ -15,13 +18,24 @@ plot_ly(data = hola, x = ~Dia, y = ~(Libros / Clientes), type = "bar") %>%
            yaxis = list(title = "Libros Por Cliente"))
 
 #Venta Libros
+hola$LibrosNuevos<- hola$Libros - hola$LibrosUsados
 plot_ly(data= hola, x = ~Dia, y = ~Libros, type = "scatter", mode = "lines", name = "Libros Vendidos") %>%
-    add_trace(x = ~dia, y = ~LibrosUsados, name = "Libros Usados") %>%
-    add_trace(x = ~dia, y = ~(Libros - LibrosUsados), name = "Libros Nuevos") %>%
+    add_trace(x = ~Dia, y = ~LibrosUsados, name = "Libros Usados") %>%
+    add_trace(x = ~Dia, y = ~LibrosNuevos, name = "Libros Nuevos") %>%
     layout(title = "Libros Vendidos")
 
 #Ganancia
-plot_ly(data = hola, x = ~Dia, y = ~Ganancia, type = "scatter", mode = "lines", name = "Ganancia Neta") %>%
+hola$GananciaNetaN<- hola$GananciaNeta - hola$GananciaNetaLU
+plot_ly(data = hola, x = ~Dia, y = ~GananciaNeta, type = "scatter", mode = "lines", name = "Ganancia Neta") %>%
     add_trace(x = ~Dia, y = ~GananciaNetaLU, name ="Ganancia Libros Usados") %>%
-    add_trace(x = ~Dia, y= ~(GananciaNeta - GananciaNetaLU), name = "Ganancia Libros Nuevos") %>%
-    layout(title = "Ganancia")
+    add_trace(x = ~Dia, y= ~GananciaNetaN, name = "Ganancia Libros Nuevos") %>%
+    layout(title = "Ganancia", yaxis = list(title = "Ganancia Neta"))
+
+# Ganancia Porcentual
+hola$GananciaP<- hola$GananciaNeta/hola$Libros
+hola$GananciaPLN<- hola$GananciaNetaN/hola$LibrosNuevos
+hola$GananciaPLU<- hola$GananciaNetaLU/hola$LibrosUsados
+plot_ly(data = hola, x = ~Dia, y = ~GananciaPLU, type = "bar", name = "Libros Usados") %>%
+    add_trace(x = ~Dia, y = ~GananciaPLN, name = "Libros Nuevos") %>%
+    add_trace(x = ~Dia, y = ~GananciaP, name = "Total Libros") %>%
+    layout(title = "Ganancia Porcentual", yaxis = list(title = "Porcentaje de ganancia"))
